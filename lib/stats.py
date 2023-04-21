9b05ba9cab21fe48544813b0e7e7d17bf509df01
Jon Coppeard <jonco@pobox.com>
2023-04-21 11:17:21 +0100

    Use pythong statistics module for calculating stats

--- lib/stats.py
+++ lib/stats.py
@@ -4,35 +4,19 @@
 
 import math
 from scipy import stats
+import statistics
 import warnings
 
 class Stats:
-    def __init__(self, results):
-        self.count = len(results)
-        self.mean, self.stdv = Stats.meanstdv(results)
-        if self.mean:
-            self.cofv = self.stdv / self.mean
-        else:
-            self.cofv = 0
-        self.min = min(results)
-        self.max = max(results)
-        self.samples = results
-
-    def meanstdv(x):
-        assert(isinstance(x, list))
-
-        # from http://www.physics.rutgers.edu/~masud/computing/WPark_recipes_in_python.html
-        n, mean, std = len(x), 0, 0
-        for a in x:
-            mean = mean + a
-        mean = mean / float(n)
-        for a in x:
-            std = std + (a - mean) ** 2
-        if n > 1:
-            std = math.sqrt(std / float(n - 1))
-        else:
-            std = 0.0
-        return mean, std
+    def __init__(self, samples):
+        self.samples = samples
+        self.count = len(samples)
+        self.min = min(samples)
+        self.max = max(samples)
+        self.median = statistics.median(samples)
+        self.mean = statistics.mean(samples)
+        self.stdv = statistics.stdev(samples, self.mean) if self.count > 1 else 0
+        self.cofv = self.stdv / self.mean if self.mean != 0 else 0
 
     def compareTo(self, other):
         return compareStats(self, other)
